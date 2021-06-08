d27556defa081f32298cd0c8f373fbf1120ccb33
Hector <hector.ibarra@quetzalsoftware.io>
2021-06-08 10:10:38 -0500

    user dashboard

---
+++ components/nav/UserDashboard.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+import styles from 'styles/Nav.module.scss';
+
+function DashboardNav() {
+  return (
+    <section className={styles.userMenu}>
+      <section className={styles.userNav}>
+        <p>Dashboard</p>
+
+        <div className={styles.linksWrap}>
+          <a href="">Drafts</a>
+          <a href="">Active</a>
+          <a href="">Closed</a>
+        </div>
+      </section>
+
+      {/* <div className={styles.btnContainer}>
+        <Link href="/admin/survey/create">
+          <a className={styles.surveyBtn}>New Survey</a>
+        </Link>
+      </div> */}
+    </section>
+  );
+}
+
+export default DashboardNav;
